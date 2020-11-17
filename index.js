62266df1f5c8572a3ae93defdc5ff500818159c4
slm747 <74074113+slm747@users.noreply.github.com>
2020-11-17 17:59:08 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function showChat() {
+    document.getElementById("chatRequest").style.display = "block";
+}
+
+function closeButton() {
+    document.getElementById("chatRequest").style.display = "none";
+}
+
